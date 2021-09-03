fe520015b424f9a59f5f7e0ee57285b649c0f867
Ronak-dot <76732355+Ronak-dot@users.noreply.github.com>
2021-09-03 18:06:13 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,31 @@
+class Iron{
+	constructor(x,y,w,h)
+	{
+		var options = {
+			restitution:0.3,
+			friction:5,
+			density:3,
+		}
+		this.x=x;
+		this.y=y;
+		this.w=w;
+        this.h=h;
+		this.body=Bodies.rectangle(this.x, this.y, this.w, this.h, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("grey");
+			rect(0,0,this.w,this.h);
+
+			pop()
+	}
+
+}
